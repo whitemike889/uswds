e56fa2ff4472ceb34060c1dd675a90b506ce9a5a
James Mejia <james.mejia@gsa.gov>
2021-07-02 15:42:52 -0500

    Create default object and reuse in variants.

--- src/components/input-test/input.stories.js
+++ src/components/input-test/input.stories.js
@@ -13,6 +13,14 @@ import "./sass/input.demo.scss";
 // https://github.com/marak/Faker.js/#api-methods
 const fullName = "Dan";
 const fullNameEg = "Ruth";
+const defaults = {
+  label: "First name",
+  value: fullName,
+  error: "",
+  placeholder: `Eg: ${fullNameEg}`,
+  isDisabled: false,
+  isRequired: false,
+}
 
 export default {
   title: "Elements/Input",
@@ -30,52 +38,30 @@ const Template = ({ label, ...args }) =>
   // You can either use a function to create DOM elements or use a plain html string!
   // return `<div>${label}</div>`;
   component({ label, ...args });
+
 export const Filled = Template.bind({});
-Filled.args = {
-  label: "First name",
-  value: fullName,
-  error: "",
-  placeholder: `Eg: ${fullNameEg}`,
-  isDisabled: false,
-  isRequired: false,
-};
+Filled.args = defaults;
 
 export const Empty = Template.bind({});
 Empty.args = {
-  label: "First name",
+  ...defaults,
   value: "",
-  error: "",
-  placeholder: `Eg: ${fullNameEg}`,
-  isDisabled: false,
-  isRequired: false,
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
-  label: "First name",
-  value: fullName,
-  error: "",
-  placeholder: `Eg: ${fullNameEg}`,
-  isDisabled: true,
-  isRequired: false,
+  ...defaults,
+  isDisabled: true
 };
 
 export const Required = Template.bind({});
 Required.args = {
-  label: "First name",
-  value: fullName,
-  error: "",
-  placeholder: `Eg: ${fullNameEg}`,
-  isDisabled: false,
+  ...defaults,
   isRequired: true,
 };
 
 export const Error = Template.bind({});
 Error.args = {
-  label: "First name",
-  value: fullName,
-  error: "Sorry, that’s a poor name. Try another.",
-  placeholder: `Eg: ${fullNameEg}`,
-  isDisabled: false,
-  isRequired: false,
+  ...defaults,
+  error: "Sorry, that’s a poor name. Try another."
 };
