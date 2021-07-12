875a6a54f6e80680269a8c93f3921bf81e7b8ba5
James Mejia <james.mejia@gsa.gov>
2021-07-12 09:33:16 -0500

    Add generated storybook dir to gitignore.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -10,26 +10,34 @@ module.exports = ({ config, mode }) => {
   // Tell Storybook where your components live
   config.resolve.alias["components"] = path.resolve(
     __dirname,
-    "../templates/components/"
+    "../src/"
   );
 
   // Add scss support
-  config.module.rules.push({
-    test: /\.scss$/,
-    use: [
-      { loader: "style-loader", options: { injectType: "styleTag" } },
-      { loader: "css-loader" },
-      { loader: "sass-loader" },
-      {
-        loader: "postcss-loader",
-        options: {
-          // Enable Source Maps
-          sourceMap: true,
+  config.module.rules.push(
+    {
+      test: /\.scss$/,
+      use: [
+        { loader: "style-loader", options: { injectType: "styleTag" } },
+        { loader: "css-loader" },
+        { loader: "sass-loader" },
+        {
+          loader: "postcss-loader",
+          options: {
+            // Enable Source Maps
+            sourceMap: true,
+          },
         },
-      },
-    ],
-    include: path.resolve(__dirname, "../"),
-  });
+      ],
+      include: path.resolve(__dirname, "../"),
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg)$/,
+      use: [
+        'file-loader',
+      ],
+    }
+  );
 
   return config;
 };
