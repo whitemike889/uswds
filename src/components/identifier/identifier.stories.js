317dab67f7a76eac1d9ed3a5700016e99e8ffff6
James Mejia <james.mejia@gsa.gov>
2021-07-15 14:01:47 -0500

    Add remaining identifier variants.

--- src/components/identifier/identifier.stories.js
+++ src/components/identifier/identifier.stories.js
@@ -77,3 +77,90 @@ const nologoArgs = {
   }
 };
 NoLogos.args = nologoArgs;
+
+export const MultipleLogos = Template.bind({});
+MultipleLogos.args = {
+  masthead: {
+    ...data.default.en.masthead,
+    aria_label: "Agency identifier,,,",
+    agency: {
+      name: `<Other agency>`,
+      shortname: `<Other agency shortname>`,
+      url: `javascript:void(0)`,
+      logo: "static/media/src/img/circle-gray-20.svg"
+    }
+  },
+  required_links: {
+    ...data.default.en.required_links,
+    aria_label: "Important links,,,"
+  },
+  usagov: {
+    ...data.default.en.usagov,
+    aria_label: "U.S. government information and services,,,"
+  }
+}
+
+export const MultipleLogosSpanish = Template.bind({});
+MultipleLogosSpanish.args = {
+  ... data.default.es,
+  masthead: {
+    ...data.default.es.masthead,
+    aria_label: "Identificador de la agencia,,,",
+    agency: {
+      name: `<Other agency>`,
+      shortname: `<Other agency shortname>`,
+      url: `javascript:void(0)`,
+      logo: "static/media/src/img/circle-gray-20.svg"
+    }
+  },
+  required_links: {
+    ...data.default.es.required_links,
+    aria_label: "Enlaces importantes,,,"
+  },
+  usagov: {
+    ...data.default.en.usagov,
+    aria_label: "Información y servicios del Gobierno de EE. UU.,,,"
+  }
+}
+
+export const TaxPayerDisclaimer = Template.bind({});
+TaxPayerDisclaimer.args = {
+  ... data.default.en,
+  masthead: {
+    ...data.default.es.masthead,
+    aria_label: "Agency identifier,,,,",
+    taxpayer_disclaimer: {
+      ...data.default.en.masthead.taxpayer_disclaimer,
+      display: true
+    }
+  },
+  required_links: {
+    ...data.default.es.required_links,
+    aria_label: "Important links,,,,"
+  },
+  usagov: {
+    ...data.default.en.usagov,
+    aria_label: "U.S. government information and services,,,,"
+  }
+}
+
+export const TaxPayerDisclaimerSpanish = Template.bind({});
+TaxPayerDisclaimerSpanish.args = {
+  ... data.default.es,
+  masthead: {
+    ...data.default.es.masthead,
+    aria_label: "Identificador de la agencia,,,,",
+    taxpayer_disclaimer: {
+      ...data.default.es.masthead.taxpayer_disclaimer,
+      display: true
+    }
+  },
+  required_links: {
+    ...data.default.es.required_links,
+    aria_label: "Enlaces importantes,,,,"
+  },
+  usagov: {
+    ...data.default.es.usagov,
+    aria_label: "Información y servicios del Gobierno de EE. UU.,,,,"
+  }
+}
